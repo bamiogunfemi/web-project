337b92427b516480272858dfc0348ae20ecd46bd
Bami Ogunfemi <ayobamiarafat09@gmail.com>
2023-12-15 12:24:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-// Function to find the square root using the Newton-Raphson method
-function squareRootNewtonRaphson(number, precision) {
-  let guess = number / 2; // Initial guess, starting at half of the number
-  let sqrt = 0; // Variable to store the square root
+// Function to find the square root of a value using the Newton-Raphson method
+function squareRootNewtonRaphson(value, precision) {
+    let guess = value / 2; // Initial guess, starting at half of the specified value
+    let sqrt = 0; // Variable to store the square root
 
-  // Loop until the guess is accurate enough based on precision
-  while (Math.abs(guess - sqrt) > precision) {
-    sqrt = guess;
-    guess = (number / guess + guess) / 2; // Newton-Raphson formula for square root
-  }
-
-  return sqrt;
+    // Loop until the guess is accurate enough to the defined precision
+    while (Math.abs(guess - sqrt) > precision) {
+        sqrt = guess;
+        guess = (value / guess + guess) / 2; // Newton-Raphson formula for square root
+    }
+    return sqrt;
 }
 
-const number = 25; // Number to find the square root of
-const precision = 0.00001; // Precision of the result
+const value = 25; // Number to find the square root of
+const precision = 0.001; // required precision to number of decimal places
 
-// Calculate the square root using the Newton-Raphson method
-const result = squareRootNewtonRaphson(number, precision);
+// Calculate the square root of a number using the Newton-Raphson method
+const answer = squareRootNewtonRaphson(value, precision);
 
-console.log(`Square root of ${number} is approximately ${result}`);
+console.log(`Square root of ${value} is approximately ${answer}`);
